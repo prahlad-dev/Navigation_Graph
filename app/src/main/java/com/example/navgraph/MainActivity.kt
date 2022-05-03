87c5380a47988c778bb6052eb462817ff1fb9fe7
prahlad-dev <73589696+prahlad-dev@users.noreply.github.com>
2022-05-03 17:49:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navgraph/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.navgraph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
